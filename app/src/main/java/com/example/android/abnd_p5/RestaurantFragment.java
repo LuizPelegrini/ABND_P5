be1e2e2e2704fc5ef1ed588edb684290a6c05898
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-10-03 21:47:38 -0300

    Restaurant item layout designed

---
+++ app/src/main/java/com/example/android/abnd_p5/RestaurantFragment.java
@@ -0,0 +1,31 @@
+package com.example.android.abnd_p5;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RestaurantFragment extends Fragment {
+
+
+    public RestaurantFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.list_layout, container, false);
+
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
