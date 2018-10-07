68297515de5b9be397efe9ce209b11c8e9ddcc08
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-10-07 20:38:08 -0300

    Added hotel pictures to the HotelFragment

--- app/src/main/java/com/example/android/abnd_p5/HotelFragment.java
+++ app/src/main/java/com/example/android/abnd_p5/HotelFragment.java
@@ -1,6 +1,7 @@
 package com.example.android.abnd_p5;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -27,13 +28,15 @@ public class HotelFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.list_layout, container, false);
 
+        Resources resources = getResources();
+
         hotels = new ArrayList<>();
-        hotels.add(new Hotel("Hotel A", Hotel.Stars.THREE, "Address A", R.drawable.ic_launcher_background));
-        hotels.add(new Hotel("Hotel B", Hotel.Stars.FIVE, "Address B", R.drawable.ic_launcher_background));
-        hotels.add(new Hotel("Hotel C", Hotel.Stars.ONE, "Address C", R.drawable.ic_launcher_background));
-        hotels.add(new Hotel("Hotel D", Hotel.Stars.TWO, "Address D", R.drawable.ic_launcher_background));
-        hotels.add(new Hotel("Hotel E", Hotel.Stars.THREE, "Address E", R.drawable.ic_launcher_background));
-        hotels.add(new Hotel("Hotel F", Hotel.Stars.FIVE, "Address F", R.drawable.ic_launcher_background));
+        hotels.add(new Hotel(resources.getString(R.string.rendezvous_hotel), Hotel.Stars.FOUR, resources.getString(R.string.rendezvous_hotel_address), R.drawable.rendezvous_hotel));
+        hotels.add(new Hotel(resources.getString(R.string.aqueen_hotel), Hotel.Stars.THREE, resources.getString(R.string.aqueen_hotel_address), R.drawable.aqueen_hotel_lavender));
+        hotels.add(new Hotel(resources.getString(R.string.strand_hotel), Hotel.Stars.THREE, resources.getString(R.string.strand_hotel_address), R.drawable.strand_hotel));
+        hotels.add(new Hotel(resources.getString(R.string.york_hotel), Hotel.Stars.FOUR, resources.getString(R.string.york_hotel_address), R.drawable.york_hotel));
+        hotels.add(new Hotel(resources.getString(R.string.regent), Hotel.Stars.FIVE, resources.getString(R.string.regent_address), R.drawable.the_regent));
+        hotels.add(new Hotel(resources.getString(R.string.marina_bay_sands_hotel), Hotel.Stars.FIVE, resources.getString(R.string.marina_bay_sands_hotel_address), R.drawable.marina_bay_sands));
 
         ListView listView = rootView.findViewById(R.id.list_view);
         HotelAdapter adapter = new HotelAdapter(getActivity(), hotels);
